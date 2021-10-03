4fb6c0059e42150a750c08e04d9610cfa09ed04f
Sheelove Dol <siesel26@gmail.com>
2021-10-02 21:10:41 -0700

    Finish greetAliens() challenge

---
+++ greetAliens.js
@@ -0,0 +1,21 @@
+// Write your code here:
+
+// Not allowed to use the .forEach() built-in method
+const greetAliens = arr => {
+  for (let item of arr) {
+    console.log(`Oh powerful ${item}, we humans offer our unconditional surrender!`);
+  }
+}
+
+
+
+
+
+
+
+// When you're ready to test your code, uncomment the below and run:
+
+const aliens = ["Blorgous", "Glamyx", "Wegord", "SpaceKing"];
+
+greetAliens(aliens);
+
