c5d2e6c0152b2c38630dbe300d6debf7a6f97a67
Sheelove Dol <siesel26@gmail.com>
2021-10-03 21:48:53 -0700

    Finish squareNums() challenge

---
+++ squareNums.js
@@ -0,0 +1,11 @@
+const numbers = [2, 7, 9, 171, 52, 33, 14]
+
+const toSquare = num => num * num
+
+// Write your code here:
+
+const squareNums = array => array.map(item => item * item)
+
+
+
+console.log(squareNums(numbers));
